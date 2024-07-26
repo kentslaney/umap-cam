4e3765634186c18660c3df165d764e390efdafa0
Kent Slaney <kent@slaney.org>
2024-07-25 18:09:52 -0700

    generalized groups

--- nnd.py
+++ nnd.py
@@ -3,24 +3,29 @@ import jax.numpy as jnp
 import jax
 
 class GroupSetter:
-    def __init__(self, group, idx, setter):
-        self.group, self.idx, self.set = group, idx, setter
+    def __init__(self, group, idx):
+        self.group, self.idx = group, idx + ((slice(None),) * 2)[len(idx):]
+
+    def set(self, value):
+        sel = range(len(self.group))[self.idx[0]]
+        sel, value = ((sel,), (value,)) if isinstance(sel, int) else (
+                sel, value * len(sel) if len(value) == 1 else value)
+        return self.group.tree_unflatten(None, tuple(
+            self.group[i].at[self.idx[1:]].set(value[sel.index(i)]) if i in sel
+            else self.group[i] for i in range(len(self.group))))
 
     def __getattr__(self, key):
-        assert callable(getattr(self.group, key))
-        return lambda *a, **kw: self.group.at[idx].set(
-                getattr(self.group.slice(idx), key)(*a, **kw))
+        f = getattr(self.group.__class__, key)
+        assert callable(f)
+        return lambda *a, **kw: self.set(
+                f(self.group[self.idx], *a, **kw))
 
 class GroupAt:
     def __init__(self, group):
         self.group = group
 
-    def __getitem__(self, idx):
-        def setter(*a):
-            updates = (self.group[i].at[idx].set(x) for i, x in enumerate(a))
-            static = (self.group[i] for i in range(len(a), len(self.group)))
-            return self.group.tree_unflatten((), (*updates, *static))
-        return GroupSetter(self.group, idx, setter)
+    def __getitem__(self, i):
+        return GroupSetter(self.group, i if isinstance(i, tuple) else (i,))
 
 class Group:
     def __new__(cls, *a, **kw):
@@ -28,57 +33,120 @@ class Group:
         return super().__new__(cls, *a, **kw)
 
     def tree_flatten(self):
-        return tuple(self), ()
+        return tuple(self), None
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         return super().__new__(cls, *children)
 
-    def slice(self, idx):
-        return super().__new__(self.__class__, *(i[idx] for i in self))
+    def __getitem__(self, idx):
+        if not isinstance(idx, tuple):
+            idx = (idx,)
+        if isinstance(range(len(self))[idx[0]], int):
+            res = super().__getitem__(idx[0])
+            return res[idx[1:]] if len(idx) > 1 else res
+        clsname = self.__class__.__name__ + "Slice"
+        dims = [
+                i for i, j in zip(self.spec._fields, idx[1:])
+                if not isinstance(j, slice)]
+        if len(dims) == 0 and range(len(self))[idx[0]] == range(len(self)):
+            if len(idx) == 1:
+                return self
+            return self.tree_unflatten(None, tuple(i[idx[1:]] for i in self))
+        dims = [i for i in self.spec._fields if i not in dims]
+        dims = dims if isinstance(self.spec, Named) else len(dims)
+        names = self._fields[idx[0]]
+        names = names if isinstance(self, Named) else len(names)
+        return grouping(clsname, dims, names)(*(
+                i[idx[1:]] if len(idx) > 1 else i
+                for i in super().__getitem__(idx[0])))
 
+    @property
     def at(self):
         return GroupAt(self)
 
     @property
     def shape(self):
-        return self[0].shape
+        return (len(self),) + self[0].shape
 
-def namedgroup(clsname, dims, named, defaults):
-    assert len(named) == len(defaults)
-    GroupSpec = namedtuple(f"{clsname}Spec", dims)
-    class GroupBase(Group, namedtuple(f"{clsname}Base", named)):
-        def __new__(cls, *a, **kw):
-            return super().__new__(cls, *(
-                    jnp.full(GroupSpec(*a, **kw), i) for i in defaults))
+    @property
+    def ndim(self):
+        return len(self.shape)
 
-        @property
-        def spec(self):
-            assert len(self) == len(GroupSpec._fields), "can't spec slice"
-            return GroupSpec(self.shape)
-
-    return type(clsname, (GroupBase,), {})
-
-class Heap(Group):
     @property
     def order(self):
         return self[0]
 
     def swapped(self, i0, i1):
-        return super().__new__(self.__class__, *(
-                i.at[i0].set(i[i1]).at[i1].set(i[i0]) for i in self))
+        i0 = i0 if isinstance(i0, tuple) else (i0,)
+        i1 = i1 if isinstance(i1, tuple) else (i1,)
+        return self.at[:, *i0].set(self[:, *i1]).at[:, *i1].set(self[:, *i0])
 
+class Named:
+    pass
+
+def nameable(clsname, names=None):
+    if names is None or isinstance(names, int):
+        class GroupSize(tuple):
+            def __new__(cls, *a):
+                assert names is None or len(a) == names
+                return super().__new__(cls, a)
+
+            @property
+            def _fields(self):
+                return range(len(self))
+        return type(clsname, (GroupSize,), {})
+    return type(clsname, (namedtuple(clsname, names), Named), {})
+
+def grouping(clsname, dims=None, names=None, defaults=None):
+    assert names is None or defaults is None or len(names) == len(defaults)
+    amount = (
+            (None if defaults is None else len(defaults)) if names is None
+            else names if isinstance(names, int) else len(names))
+    GroupSpec = nameable(f"{clsname}Spec", dims)
+    Container = nameable(f"{clsname}Base", amount if names is None else names)
+    class GroupBase(Group, Container):
+        def __new__(cls, *a, **kw):
+            if defaults is None:
+                checking = (i for i in a + tuple(kw.values()))
+                against = next(checking).shape
+                assert dims is None or len(against) == (
+                        dims if isinstance(dims, int) else len(dims))
+                assert all(i.shape == against for i in checking)
+                return super().__new__(cls, *a, **kw)
+            return super().__new__(cls, *(
+                    jnp.full(GroupSpec(*a, **kw), i) for i in defaults))
+
+        @property
+        def spec(self):
+            return GroupSpec(*self.shape[1:])
+
+        @classmethod
+        def grouped(cls, *a, names=None, sized=True):
+            assert names is None or sized is True
+            assert all(isinstance(i, cls) for i in a)
+            condition = f"Of{len(a)}" if sized or names is not None else ""
+            return grouping(
+                    f"{clsname}Group{condition}",
+                    None if dims is None else (clsname,) + dims,
+                    names if names is not None or not sized else len(a))(*a)
+
+    return type(clsname, (GroupBase,), {})
+
+class Heap(Group):
     def sifted(self, i):
         sel = (lambda x, y: x, lambda x, y: y)
-        def cond(heap, i, broken):
-            return not broken and i * 2 + 1 < heap.shape[0]
+        def cond(args):
+            heap, i, broken = args
+            return ~broken & (i * 2 + 1 < heap.order.shape[0])
 
-        def inner(heap, i, broken):
+        def inner(args):
+            heap, i, broken = args
             left, right, swap = i * 2 + 1, i * 2 + 2, i
             lpivot = heap.order[swap] < heap.order[left]
             swap = jax.lax.cond(lpivot, *sel, left,  swap)
-            rpivot = right < heap.shape[0] and \
-                    heap.order[swap] < heap.order[right]
+            rpivot = (right < heap.order.shape[0]) & (
+                    heap.order[swap] < heap.order[right])
             swap = jax.lax.cond(rpivot, *sel, right, swap)
 
             broken = swap == i
@@ -92,48 +160,49 @@ class Heap(Group):
 
     def sorted(self):
         heap = self
-        for i in range(self.shape[0]):
-            for j in range(self.shape[1] - 1, 0, -1):
-                heap = heap.swapped((i, 0), (i, j)).sifted(0)
+        for i in range(self.shape[1]): # TODO: vmap? too large for jit to unroll
+            for j in range(self.shape[2] - 1, 0, -1):
+                heap = heap.swapped((i, 0), (i, j)).at[:, i, :j].sifted(0)
         return heap
 
     def ref(self, key):
         return getattr(self, key) if isinstance(type(key), str) else self[key]
 
     def push(self, *value, checked=()):
-        lim = len(value)
-        values = value + (None,) * (len(self) - lim)
-        ins = super().__new__(self.__class__, *values)
+        value = tuple(map(jnp.asarray, value))
+        ins = self.at[:len(value), 0].set(value)[:len(value), 0]
         res = ins.order < self.order[0] and all((
                 jnp.all(ins.ref(i)[None] != self.ref(i)) for i in checked))
 
         def init(heap):
-            heap = heap.at[0].set(values)
+            heap = heap.at[:len(value), 0].set(value)
             _, i, heap = jax.lax.while_loop(
-                    lambda c, *a: c, inner, (True, 0, heap))
-            return heap.at[i].set(values)
+                    lambda a: a[0], inner, (True, 0, heap))
+            return heap.at[:len(value), i].set(value)
 
-        def inner(continues, i, heap):
+        def inner(args):
+            continues, i, heap = args
             left, right = 2 * i + 1, 2 * i + 2
-            loob = left >= heap.shape[0]
-            roob = right >= heap.shape[0]
+            loob = left >= heap.order.shape[0]
+            roob = right >= heap.order.shape[0]
             flipped = lambda: heap.order[left] >= heap.order[right]
-            pivot = jax.lax.cond(roob or flipped(), lambda: left, lambda: right)
-            swapping = not loob and ins.order < heap.order[pivot]
+            pivot = jax.lax.cond(roob | flipped(), lambda: left, lambda: right)
+            swapping = ~loob & (ins.order < heap.order[pivot])
             return (swapping,) + jax.lax.cond(
                     swapping,
-                    lambda: (pivot, heap.at[i].set(*heap.slice(i_swap)[:lim])),
+                    lambda: (pivot, heap.at[:, i].set(heap[:, pivot])),
                     lambda: (i, heap))
         return res, jax.lax.cond(res, init, lambda a: a, self)
 
+    def pusher(self, *a, **kw):
+        return __class__.push(self, *a, **kw)[1]
+
 @jax.tree_util.register_pytree_node_class
-class NNDHeap(Heap, namedgroup(
-        "Heap", ("points", "size"), ("distances", "indices", "flags"),
-        (jnp.int32(-1), jnp.float32(jnp.inf), jnp.uint8(0)))):
-    def build(self, max_candidates, rng_state):
-        cur = Heap(self.spec.points, max_candidates)
-        old = Heap(self.spec.points, max_candidates)
-        for i in range(self.spec.points): # jit unrolling inefficiency; vmap
-            for j in range(self.spec.size):
-                pass
+class NNDHeap(Heap, grouping(
+        "NNDHeap", ("points", "size"), ("distances", "indices", "flags"),
+        (jnp.float32(jnp.inf), jnp.int32(-1), jnp.uint8(0)))):
+    def build(self, limit, rng_state):
+        update = self.grouped(
+                *((self.__new__(self.__class__, self.spec.points, limit),) * 2),
+                names=("old", "new"))
 
